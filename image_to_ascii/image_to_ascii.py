b9807c85875b9e483cc9a66c7334c436dca0b852
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-07-27 20:48:46 +0200

    Script almost works

---
+++ image_to_ascii/image_to_ascii.py
@@ -0,0 +1,92 @@
+from PIL import Image
+
+IMAGE_NAME = "fantasia"
+IMAGE_EXTENSION = ".jpg"
+NEW_SIZE_RATIO = 10
+
+def main() -> None:
+
+    img_name = IMAGE_NAME
+    img_extension = IMAGE_EXTENSION
+
+    img = Image.open(f"{img_name}{img_extension}")
+    if not img:
+        print("Image not found")
+        return
+    
+    width, height = img.size
+
+    # ASCII characters are much bigger than a pixel, so it's necesary to resize the image
+    
+    new_width = width // NEW_SIZE_RATIO
+    aspect_ratio = height / width
+    new_height = int(new_width * aspect_ratio * 0.5)
+    img = img.resize((new_width, new_height))
+    width, height = img.size
+
+
+    grid = []
+    for _ in range (height):
+        grid.append([" "] * width)
+
+    ascii_characters = {
+    '@': 0,
+    '#': 70,
+    '8': 140,
+    '&': 210,
+    '%': 280,
+    '$': 350,
+    '*': 420,
+    '+': 490,
+    '=': 560,
+    '-': 600,
+    ':': 650,
+    '.': 720
+}
+
+    img = img.convert("RGB") # Converts each pixel to an RGB tuple (0 - 255, 0 - 255, 0 - 255)
+    pix = img.load() # Loads all the pixels
+
+    for y in range(height):
+        for x in range(width):
+            r, g, b = pix[x, y]
+            brightness = 0.299*r + 0.587*g + 0.114*b
+            scaled_brightness = brightness * (700 / 255)
+
+
+            if scaled_brightness <= ascii_characters['@']:
+                grid[y][x] = '@'
+            elif scaled_brightness <= ascii_characters['#']:
+                grid[y][x] = '#'
+            elif scaled_brightness <= ascii_characters['8']:
+                grid[y][x] = '8'
+            elif scaled_brightness <= ascii_characters['&']:
+                grid[y][x] = '&'
+            elif scaled_brightness <= ascii_characters['%']:
+                grid[y][x] = '%'
+            elif scaled_brightness <= ascii_characters['$']:
+                grid[y][x] = '$'
+            elif scaled_brightness <= ascii_characters['*']:
+                grid[y][x] = '*'
+            elif scaled_brightness <= ascii_characters['+']:
+                grid[y][x] = '+'
+            elif scaled_brightness <= ascii_characters['=']:
+                grid[y][x] = '='
+            elif scaled_brightness <= ascii_characters['-']:
+                grid[y][x] = '-'
+            elif scaled_brightness <= ascii_characters[':']:
+                grid[y][x] = ':'
+            elif scaled_brightness <= ascii_characters['.']:
+                grid[y][x] = '.'
+            else:
+                grid[y][x] = ' '
+
+
+    with open(f"{img_name}.txt", "w") as new_image:
+        for row in grid:
+            new_image.write("".join(row)+"\n")
+        print("All done! :)")
+
+
+if __name__ == "__main__":
+    main()
